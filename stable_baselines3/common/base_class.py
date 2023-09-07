36da7057ac93aa8f15e7d99afbd0d1a57df53a73
Adrià Garriga-Alonso <adria@far.ai>
2023-09-07 16:32:36 -0700

    last masks typo

--- stable_baselines3/common/base_class.py
+++ stable_baselines3/common/base_class.py
@@ -552,7 +552,7 @@ class BaseAlgorithm(ABC):
 
         :param observation: the input observation
         :param state: The last hidden states (can be None, used in recurrent policies)
-        :param episode_start: The lastmasks (can be None, used in recurrent policies)
+        :param episode_start: The last masks (can be None, used in recurrent policies)
             this correspond to beginning of episodes,
             where the hidden states of the RNN must be reset.
         :param deterministic: Whether or not to return deterministic actions.
