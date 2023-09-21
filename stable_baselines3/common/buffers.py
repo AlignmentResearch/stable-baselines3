dc8932aed964520bca47835e152adc4b872e82fd
Adrià Garriga-Alonso <adria@far.ai>
2023-09-21 15:08:48 -0700

    next_is_non_terminal

--- stable_baselines3/common/buffers.py
+++ stable_baselines3/common/buffers.py
@@ -454,13 +454,13 @@ class RolloutBuffer(BaseBuffer):
         last_gae_lam: Union[float, th.Tensor] = 0.0
         for step in reversed(range(self.buffer_size)):
             if step == self.buffer_size - 1:
-                next_non_terminal = ~dones
+                next_is_non_terminal = ~dones
                 next_values = last_values
             else:
-                next_non_terminal = ~self.episode_starts[step + 1]
+                next_is_non_terminal = ~self.episode_starts[step + 1]
                 next_values = self.values[step + 1]
-            delta = self.rewards[step] + self.gamma * next_values * next_non_terminal - self.values[step]
-            last_gae_lam = delta + self.gamma * self.gae_lambda * next_non_terminal * last_gae_lam
+            delta = self.rewards[step] + self.gamma * next_values * next_is_non_terminal - self.values[step]
+            last_gae_lam = delta + self.gamma * self.gae_lambda * next_is_non_terminal * last_gae_lam
             self.advantages[step] = last_gae_lam
         # TD(lambda) estimator, see Github PR #375 or "Telescoping in TD(lambda)"
         # in David Silver Lecture 4: https://www.youtube.com/watch?v=PnHCvfgC_ZA
