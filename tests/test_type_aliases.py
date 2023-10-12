b2e8fa167a3f784aa0c3f2ac8c10b5f3b27a4d0b
Adrià Garriga-Alonso <adria@far.ai>
2023-10-11 19:11:25 -0700

    test Uppercase types too

--- tests/test_type_aliases.py
+++ tests/test_type_aliases.py
@@ -1,3 +1,5 @@
+from typing import Dict, List
+
 import pytest
 
 from stable_baselines3.common.type_aliases import check_cast, non_null
@@ -14,9 +16,11 @@ def test_non_null():
 def test_check_cast():
     assert check_cast(dict, {}) == {}
     assert check_cast(dict[str, int], {}) == {}
+    assert check_cast(Dict[str, int], {}) == {}
 
     with pytest.raises(TypeError):
         check_cast(list[int], {})
+        check_cast(List[int], {})
 
     # NOTE: check_cast does not check the template arguments, only the main class.
     a: list[str] = ["a"]
