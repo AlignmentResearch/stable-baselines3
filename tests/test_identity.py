62329d79a484f83db64cacf11ca5fd4d2b4402e3
Adrià Garriga-Alonso <adria@far.ai>
2024-01-31 11:37:16 -0800

    RecurrentPPO at 10k

--- tests/test_identity.py
+++ tests/test_identity.py
@@ -23,7 +23,7 @@ DIM = 4
 def test_discrete(model_class, env):
     env_ = DummyVecEnv([lambda: env])
     kwargs: dict[str, Any] = {}
-    n_steps = 25000 if model_class == RecurrentPPO else 10000
+    n_steps = 10000
     if model_class == DQN:
         kwargs = dict(learning_starts=0)
         # DQN only support discrete actions
